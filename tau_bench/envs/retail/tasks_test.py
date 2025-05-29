ebd32ae3d50a04d6f6980090be89ab9febdc1e93
LiMFV <hoang.ngoc.linh@moneyforward.co.jp>
2025-05-29 21:52:33 +0700

    Add

--- tau_bench/envs/retail/tasks_test.py
+++ tau_bench/envs/retail/tasks_test.py
@@ -3360,4 +3360,53 @@ TASKS_TEST = [
         ],
         outputs=[],
     ),
+    Task(
+        annotator="4",
+        user_id="harper_santos_8115",
+        instruction="Your name is Santos Harper and your email is harper.santos8390@example.com. "
+        "You want to update your email to harper.santos1010@example.com to receive notifications. "
+        "You received consultation from an agent about the laptop you ordered under order number #W4941028, "
+        "and you would like to exchange it for a new one with an i7 processor, 8GB RAM, and 1TB SSD. "
+        "You want to know how much you need to pay today in total, and you also want to cancel other orders "
+        "and find out the total amount you can get refunded.",
+        actions=[
+            Action(
+                name="find_user_id_by_email",
+                kwargs={"email": "harper.santos8390@example.com"},
+            ),
+            Action(
+                name="modify_user_email",
+                kwargs={
+                    "user_id": "harper_santos_8115",
+                    "email": "harper.santos1010@example.com",
+                },
+            ),
+            Action(
+                name="get_order_details",
+                kwargs={"order_id": "#W4941028"},
+            ),
+            Action(
+                name="get_product_details",
+                kwargs={"product_id": "#4760268021"},
+            ),
+            Action(
+                name="exchange_delivered_order_items",
+                kwargs={
+                    "order_id": "#W4941028",
+                    "item_ids": ["3265035808"],
+                    "new_item_ids": ["9844888101"],
+                    "payment_method_id": "paypal_2870241",
+                },
+            ),
+            Action(
+                name="cancel_pending_order",
+                kwargs={"order_id": "#W6629830", "reason": "no longer needed"},
+            ),
+            Action(
+                name="calculate",
+                kwargs={"expression": "3997.94"},
+            ),
+        ],
+        outputs=[],
+    ),
 ]
