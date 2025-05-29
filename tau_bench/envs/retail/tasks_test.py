1278ccaa66018911e0421fee042f3d439a66ce5f
LiMFV <hoang.ngoc.linh@moneyforward.co.jp>
2025-05-29 11:06:21 +0700

    Add tast_test_cancel_order

--- tau_bench/envs/retail/tasks_test.py
+++ tau_bench/envs/retail/tasks_test.py
@@ -3346,4 +3346,18 @@ TASKS_TEST = [
         ],
         outputs=[],
     ),
+    Task(
+        annotator="4",
+        user_id="harper_santos_8115",
+        instruction="You name is Santos Harper and your email is harper.santos8390@example.com. You wanna cancel #W5765742 order because you change your mind. You don't want to reveal the reason until the agent asks. You'd say ordered by mistake if asked.",
+        actions=[
+            Action(
+                name="find_user_id_by_email",
+                kwargs={"email": "harper.santos8390@example.com"},
+            ),
+            Action(name="get_user_details", kwargs={"user_id": "harper_santos_8115"}),
+            Action(name="get_order_details", kwargs={"order_id": "#W5765742"}),
+        ],
+        outputs=[],
+    ),
 ]
